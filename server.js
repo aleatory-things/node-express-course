47036a9f890a5a0fad4b7102759f1d0d3d75c8b1
Katacoda Scenario <scenario@katacoda.com>
2021-06-21 23:48:11 +0000

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000,function(){
+const mockUserData = [
+    {name: "Mark"},
+    {name: "Jill"}
+]
+
+app.get('/users', (req, res) => {
+    res.json(
+        {
+            sucess: true,
+            message: "Successfully get users",
+            users: mockUserData
+        }
+    )
+})
+
+app.listen(8000,function(){
     console.log("server is running")
 })
