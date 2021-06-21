162ec9b8762ea97e2fafca30861513e3367c08eb
Katacoda Scenario <scenario@katacoda.com>
2021-06-21 23:55:04 +0000

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+
 const mockUserData = [
     {name: "Mark"},
     {name: "Jill"}
 ]
+
+app.use(bodyParser.json());
+
+app.listen(8000,function(){
+    console.log("server is running")
+})
 
 app.get('/users', (req, res) => {
     res.json(
@@ -26,6 +35,25 @@ app.get('/users/:id',function(req,res){
 })
 
 
-app.listen(8000,function(){
-    console.log("server is running")
+
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
